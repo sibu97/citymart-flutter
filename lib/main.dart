e17753ee9cd1aa641eec4c6487501525e15dddfb
Sibananda Nayak <nayaksibananda22@gmail.com>
2024-04-03 18:17:07 +0530

    my first commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,19 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
+        // backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: const Text('Sibananda Nayak',
-              style: TextStyle(color: Colors.white)),
-          backgroundColor: Colors.blueGrey[900],
+          leading: Padding(
+            padding: EdgeInsets.only(left: 1.0), // Set the left margin
+            child: Image.asset(
+              'images/citymart-app-logo.png',
+
+              width: 40, // Set the width of the image
+              height: 40,
+            ),
+          ),
+          title: const Text('CityMart', style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blue,
         ),
       ),
     ),
